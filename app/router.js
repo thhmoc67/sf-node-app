c01b63b613cc9f28a160e1047772f6d5e473b85b
Dayal <dayal.sharan@nestaway.com>
2019-04-04 15:30:36 +0530

    twillio removed

--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
 var indexRouter = require("./controllers");
 var usersRouter = require("./controllers/users");
-var otpRouter = require("./controllers/otp");
+// var otpRouter = require("./controllers/otp");
 
 const router = app => {
   app.use("/", indexRouter);
   app.use("/users", usersRouter);
-  app.use("/otp",otpRouter)
+  // app.use("/otp",otpRouter)
 };
 
 module.exports = router
